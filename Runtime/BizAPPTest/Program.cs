98e2766f16173b6d16d99a58d54cad6efe40fffc
APPPOINT\ksaravan <ksara@apppoint.com>
2020-08-03 13:12:08 +0530

    Added test program

---
+++ Runtime/BizAPPTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using BizAPP.Common;
+using BizAPP.Runtime.Core;
+using BizAPP.Runtime.Core.Service.Authentication;
+
+namespace BizAPPTest
+{
+	class Program
+	{
+		static void Main( string[ ] args )
+		{
+			var settings = ConfigurationManager.AppSettings;
+			string enterprise = settings[ "enterprise" ];
+			string user = settings[ "username" ];
+			string pwd = settings[ "password" ];
+			var rb = RuntimeBootstrap.GetTheOnlyBootstrap( settings[ "hosting" ], settings[ "registry" ] );
+
+			Hashtable creds = new Hashtable( 4 );
+			creds.Add( AuthenticationParameters.ENTERPRISENAME, enterprise );
+			creds.Add( AuthenticationParameters.REALM, "BizAPP" );
+			creds.Add( AuthenticationParameters.USERNAME, user );
+			creds.Add( AuthenticationParameters.PASSWORD, pwd );
+
+			try
+			{
+				using ( var session = rb.GetSession( creds, NetworkInformation.Current, BizAPP.Runtime.Core.Service.Session.SessionMode.Client ) )
+				{
+					// do your stuff here
+				}
+			}
+			catch ( Exception ex )
+			{
+				Console.WriteLine( ExceptionUtils.CaptureCompleteException( ex ) );
+			}
+		}
+	}
+}
---
+++ Runtime/BizAPPTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "BizAPPTest" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "BizAPPTest" )]
+[assembly: AssemblyCopyright( "Copyright ©  2013" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "9e63c4da-60cb-4864-bf7a-c7ec1b468d99" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
